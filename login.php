38bce5f9069658c17208b521ec02bbc9bf96a411
dawid-soszynski <u20_dawidsoszynski@zsp1.siedlce.pl>
2024-10-29 17:23:26 +0100

    a

--- login.php
+++ login.php
@@ -5,37 +5,21 @@ require_once 'db_connection.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
-    $is_admin = false;
 
-    $stmt = $pdo->prepare("SELECT password FROM users WHERE username = :username");
+    $stmt = $pdo->prepare("SELECT password, role FROM users WHERE username = :username");
     $stmt->execute(['username' => $username]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($row) {
-        $hashed_password = $row['password'];
-        if (password_verify($password, $hashed_password)) {
-            $_SESSION['username'] = $username;
+    if ($row && password_verify($password, $row['password'])) {
+        $_SESSION['username'] = $username;
+        $_SESSION['role'] = $row['role'];
+        $_SESSION['is_admin'] = $row['role'] === 'admin';
 
-            if ($username == 'admin') {
-                $is_admin = true;
-                $_SESSION['is_admin'] = 1;
-            }
-            else {
-                $_SESSION['is_admin'] = 0;
-            }
-
-            echo json_encode(['success' => true, 'message' => 'Zalogowano pomyślnie.', 'username' => $username, 'is_admin' => $is_admin]);
-            exit();
-        } else {
-            echo json_encode(['success' => false, 'message' => 'Błędna nazwa użytkownika lub hasło.']);
-            exit();
-        }
+        echo json_encode(['success' => true, 'message' => 'Zalogowano pomyślnie.', 'role' => $row['role']]);
     } else {
         echo json_encode(['success' => false, 'message' => 'Błędna nazwa użytkownika lub hasło.']);
-        exit();
     }
 } else {
     echo json_encode(['success' => false, 'message' => 'Nieprawidłowe żądanie.']);
-    exit();
 }
 ?>
