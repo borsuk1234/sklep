578506ed031ea8056ced3e30d949dfcb75882700
dawid-soszynski <u20_dawidsoszynski@zsp1.siedlce.pl>
2024-11-27 15:37:36 +0100

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -1,14 +1,13 @@
 <?php
-$host = 'mysql.ct8.pl';
-$dbname = 'm50640_sklep';
-$username = 'm50640_sklep';
-$password = 'JacekSwider123@';
+$host = 'localhost';
+$dbname = 'sklep';
+$username = 'root';
+$password = '';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo json_encode(['success' => false, 'message' => 'Błąd połączenia z bazą danych: ' . $e->getMessage()]);
-    exit();
+    die("Błąd połączenia z bazą danych: " . $e->getMessage());
 }
 ?>
